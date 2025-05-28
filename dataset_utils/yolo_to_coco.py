9e31416d22838cd1f01c9b5c764d6fb4c763455c
roee <roee@lulav.space>
2025-05-28 15:04:02 +0300

    coco dataset utils
    preparing for generating a better dataset

--- dataset_utils/yolo_to_coco.py
+++ dataset_utils/yolo_to_coco.py
@@ -1,126 +1,145 @@
-import os
 import json
-import cv2
-import glob
-
-def convert_yolo_to_coco(yolo_img_dir, yolo_label_dir, class_list, output_json_path):
-    categories = [{"id": i, "name": name, "supercategory": "none"} for i, name in enumerate(class_list)]
-
-    images = []
-    annotations = []
-    ann_id = 1
-    img_id = 0
-
-    for filename in os.listdir(yolo_label_dir):
-        if not filename.endswith('.txt'):
-            continue
-
-        image_name = filename.replace('.txt', '.jpg')  # or .png
-        image_path = os.path.join(yolo_img_dir, image_name)
-        label_path = os.path.join(yolo_label_dir, filename)
-
-        if not os.path.exists(image_path):
-            continue
-
-        img = cv2.imread(image_path)
-        h, w = img.shape[:2]
-
-        images.append({
-            "file_name": image_name,
-            "height": h,
-            "width": w,
-            "id": img_id
-        })
-
-        with open(label_path, 'r') as f:
-            for line in f.readlines():
-                cls_id, x, y, bw, bh = map(float, line.strip().split())
-                x_min = int((x - bw / 2) * w)
-                y_min = int((y - bh / 2) * h)
-                width = int(bw * w)
-                height = int(bh * h)
-
-                annotations.append({
-                    "id": ann_id,
-                    "image_id": img_id,
-                    "category_id": int(cls_id),
-                    "bbox": [x_min, y_min, width, height],
-                    "area": width * height,
-                    "iscrowd": 0
-                })
-                ann_id += 1
-
-        img_id += 1
-
-    coco_dict = {
-        "images": images,
-        "annotations": annotations,
-        "categories": categories
-    }
-
-    with open(output_json_path, 'w') as out_file:
-        json.dump(coco_dict, out_file, indent=4)
+import os
+from pathlib import Path
+from PIL import Image
+from typing import List, Dict, Union
 
 
+class CocoDatasetManager:
+    def __init__(self):
+        self.data = {
+            "images": [],
+            "annotations": [],
+            "categories": []
+        }
+        self.image_id_counter = 1
+        self.annotation_id_counter = 1
+        self.filename_to_image_id = {}
 
-# def split_yolo_dataset():
-#     import glob
-#     import re
-#     import shutil
-#
-#     base_yolo_dir = '/home/roee/Downloads/Drone Dataset.v2i.yolov8'
-#
-#     yolo_img_dirs = [os.path.join(base_yolo_dir, 'test', 'images'),
-#                      os.path.join(base_yolo_dir, 'train', 'images'),
-#                      os.path.join(base_yolo_dir, 'valid', 'images')]
-#
-#     yolo_label_dirs = [os.path.join(base_yolo_dir, 'test', 'labels'),
-#                      os.path.join(base_yolo_dir, 'train', 'labels'),
-#                      os.path.join(base_yolo_dir, 'valid', 'labels')]
-#
-#     for d in yolo_img_dirs:
-#         img_files = glob.glob(os.path.join(d,'*.jpg'))
-#         for img_file in img_files:
-#
-#             sp = re.split(r'[_,-]+', os.path.basename(img_file))
-#
-#             scen_name = '{:4d}{:02d}{:02d}_{:02d}{:02d}{:02d}'.format(int(sp[1]), int(sp[2]), int(sp[3]), int(sp[4]), int(sp[5]), int(sp[6]))
-#             res_img_name = scen_name + '_' + sp[7] + '.jpg'
-#
-#             res_folder = os.path.join(base_yolo_dir, scen_name, 'images')
-#             if not os.path.isdir(res_folder):
-#                 os.makedirs(res_folder)
-#             shutil.copy(os.path.join(d,img_file), os.path.join(res_folder,res_img_name))
-#
-#
-#     for d in yolo_label_dirs:
-#         lbl_files = glob.glob(os.path.join(d,'*.txt'))
-#         for lbl_file in lbl_files:
-#             sp = re.split(r'[_,-]+', os.path.basename(lbl_file))
-#
-#             scen_name = '{:4d}{:02d}{:02d}_{:02d}{:02d}{:02d}'.format(int(sp[1]), int(sp[2]), int(sp[3]), int(sp[4]), int(sp[5]), int(sp[6]))
-#             res_name = scen_name + '_' + sp[7] + '.txt'
-#
-#             res_folder = os.path.join(base_yolo_dir, scen_name, 'labels')
-#             if not os.path.isdir(res_folder):
-#                 os.makedirs(res_folder)
-#             shutil.copy(os.path.join(d,lbl_file), os.path.join(res_folder,res_name))
+    def load_coco(self, json_path: Union[str, Path, List]):
+        if isinstance(json_path, str) or isinstance(json_path, Path):
+            json_path = [json_path]
+
+        for jp in json_path:
+            if os.path.isfile(jp):
+                raise Exception('json file not found! {}'.format(jp))
+
+            with open(jp, "r") as f:
+                coco = json.load(f)
+
+            if not self.data["categories"]:
+                self.data["categories"] = coco.get("categories", [])
+
+            for img in coco.get("images", []):
+                new_img = img.copy()
+                new_img["id"] = self.image_id_counter
+                self.filename_to_image_id[img["file_name"]] = self.image_id_counter
+                self.data["images"].append(new_img)
+                self.image_id_counter += 1
+
+            for ann in coco.get("annotations", []):
+                new_ann = ann.copy()
+                old_img = next((i for i in coco["images"] if i["id"] == ann["image_id"]), None)
+                if old_img is None:
+                    continue
+                new_ann["id"] = self.annotation_id_counter
+                new_ann["image_id"] = self.filename_to_image_id[old_img["file_name"]]
+                self.data["annotations"].append(new_ann)
+                self.annotation_id_counter += 1
+
+        return
+
+    def load_yolo_dataset(self, image_dir: str, label_dir: str, class_names: List[str]):
+        if not self.data["categories"]:
+            self.data["categories"] = [{"id": i, "name": name, "supercategory": "none"} for i, name in enumerate(class_names)]
+
+        for image_file in Path(image_dir).glob("*.*"):
+            label_file = Path(label_dir) / f"{image_file.stem}.txt"
+            if not label_file.exists():
+                continue
+
+            with Image.open(image_file) as img:
+                width, height = img.size
+
+            image_id = self.image_id_counter
+            self.filename_to_image_id[image_file.name] = image_id
+            self.data["images"].append({
+                "id": image_id,
+                "file_name": image_file.name,
+                "width": width,
+                "height": height
+            })
+            self.image_id_counter += 1
+
+            with open(label_file, "r") as f:
+                for line in f:
+                    parts = line.strip().split()
+                    if len(parts) != 5:
+                        continue
+                    class_id, x_center, y_center, w, h = map(float, parts)
+                    x_center *= width
+                    y_center *= height
+                    w *= width
+                    h *= height
+                    x_min = x_center - w / 2
+                    y_min = y_center - h / 2
+
+                    self.data["annotations"].append({
+                        "id": self.annotation_id_counter,
+                        "image_id": image_id,
+                        "category_id": int(class_id),
+                        "bbox": [x_min, y_min, w, h],
+                        "area": w * h,
+                        "iscrowd": 0
+                    })
+                    self.annotation_id_counter += 1
+
+    def save_yaml(self, output_yaml: Union[str, Path], dataset_root: Union[str, Path]):
+        """
+        Save a .yaml config file for YOLO training.
+        Note: this does NOT copy or move any files.
+        """
+        yaml_dict = {
+            "path": str(dataset_root),
+            "train": "images/train",  # user should adjust paths if needed
+            "val": "images/val",
+            "names": {cat["id"]: cat["name"] for cat in self.data["categories"]}
+        }
+        with open(output_yaml, "w") as f:
+            for key, val in yaml_dict.items():
+                if isinstance(val, dict):
+                    f.write(f"{key}:\n")
+                    for k, v in val.items():
+                        f.write(f"  {k}: {v}\n")
+                else:
+                    f.write(f"{key}: {val}\n")
+        print(f"Saved YOLO .yaml config to {output_yaml}")
+
+    def export_to_json(self, output_path: Union[str, Path]):
+        with open(output_path, "w") as f:
+            json.dump(self.data, f, indent=4)
+        print(f"Exported COCO dataset to {output_path}")
 
 
 if __name__ == '__main__':
+    # Initialize manager
+    manager = CocoDatasetManager()
 
-    # split_yolo_dataset()
+    # Load multiple COCO datasets
+    manager.load_multiple_coco([
+        "dataset1/annotations/instances_train.json",
+        "dataset2/annotations/instances_train.json"
+    ])
 
-    # Example usage:
-    yolo_base_dir = '/home/roee/Downloads/Drone Dataset.v2i.yolov8/dataset_baseline'
-    class_list = ['fixed_wing']  # or load from .names file
+    # Load a YOLO dataset
+    manager.load_yolo_dataset(
+        image_dir="yolo/images/train",
+        label_dir="yolo/labels/train",
+        class_names=["cat", "dog", "person"]
+    )
 
-    scenario_dirs = glob.glob(os.path.join(yolo_base_dir,'*','*'))
+    # Save merged dataset as COCO JSON
+    manager.export_to_json("merged_coco.json")
 
-    for scen_dir in scenario_dirs:
-        yolo_img_dir = os.path.join(scen_dir, 'images')
-        yolo_label_dir = os.path.join(scen_dir, 'labels')
-        if not os.path.isdir(yolo_img_dir) or not os.path.isdir(yolo_label_dir):
-            raise Exception('folder not found!')
-        output_json_path = os.path.join(scen_dir, 'data.json')
-        convert_yolo_to_coco(yolo_img_dir, yolo_label_dir, class_list, output_json_path)
+    # Save YAML config for YOLO training
+    manager.save_yaml("dataset.yaml", dataset_root="/path/to/data")
