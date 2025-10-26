285d1ec47b3d059369327cbad8fb50b42e450618
roee <roee@lulav.space>
2025-10-26 13:16:53 +0200

    yolo openvino export update
    just add prints

--- yolo_detector/export_openvino.py
+++ yolo_detector/export_openvino.py
@@ -19,5 +19,22 @@ file_name = os.path.basename(onnx_model_path)
 xml_file_name = file_name[:-5] + '_openvino.xml'
 from openvino.runtime import serialize
 serialize(ir_model, str(output_dir / xml_file_name))
-
 print("OpenVINO IR saved to:", output_dir)
+
+# Load ONNX-openvino model
+from openvino.runtime import Core
+output_model_xml_file = os.path.join(output_dir, xml_file_name)
+output_model_bin_file = output_model_xml_file.replace('.xml','.bin')
+
+ie = Core()
+model = ie.read_model(model=output_model_xml_file, weights=output_model_bin_file)
+
+# Inspect ops / types on the uncompiled model
+# for op in model.get_ops():
+#     for i, out in enumerate(op.outputs):
+#         print(op.get_type_name(), op.get_friendly_name(), out.get_element_type())
+
+compiled_model = ie.compile_model(model=model, device_name="CPU")
+print(compiled_model.input(0))
+print(compiled_model.output(0))
+
